1dbc5425394dcd0da4bd0a10e310d021fadea4b1
Ales Kozelsky <ales.kozelsky@gmail.com>
2026-04-01 18:48:47 +0200

    big refactor

---
+++ apps/nextjs-docs/app/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  "index": "Getting Started",
+  'architecture': 'Architecture',
+  'asset-pipeline': 'Asset Pipeline',
+  'react-integration': 'React Integration',
+  'ai-integration': 'AI Integration'
+}
